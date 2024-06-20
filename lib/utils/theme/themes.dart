89c9990e49ecdfdc6112d40e2c07a354ea9fb1b7
Ronald <ron314954@gmail.com>
2024-06-19 22:09:31 -0400

    fix theme problems

--- lib/utils/theme/themes.dart
+++ lib/utils/theme/themes.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
 
-var PRIMARY_COLOR = Colors.purple;
+var PRIMARY_COLOR = Colors.purple.shade200;
 var PRIMARY_DARK_COLOR = Colors.deepPurple.shade700;
 var FOCUS_COLOR = Colors.indigo;
 var ACCENT_COLOR = Colors.deepPurpleAccent.shade200;
 var ERROR_COLOR = Colors.red.shade700;
 
 ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  primaryColor: PRIMARY_COLOR.shade200,
-  focusColor: FOCUS_COLOR,
-  appBarTheme: AppBarTheme(
-    backgroundColor: ACCENT_COLOR
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: PRIMARY_DARK_COLOR,
+    brightness: Brightness.light,
   ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ButtonStyle(
-        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-            const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0)),
-        shape: MaterialStateProperty.all<OutlinedBorder>(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
-        backgroundColor: MaterialStateProperty.all<Color>(ACCENT_COLOR)),
+      padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+          const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0)),
+      shape: WidgetStateProperty.all<OutlinedBorder>(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
+    ),
   ),
   outlinedButtonTheme: OutlinedButtonThemeData(
     style: ButtonStyle(
-      textStyle: MaterialStateProperty.all<TextStyle>(TextStyle(color: ACCENT_COLOR)),
-        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-            const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0)),
-        shape: MaterialStateProperty.all<OutlinedBorder>(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
-        ),
-  ),
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
-    backgroundColor: ACCENT_COLOR,
+      padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+          const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0)),
+      shape: WidgetStateProperty.all<OutlinedBorder>(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
+    ),
   ),
 );
 
-ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+ThemeData darkTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+      seedColor: PRIMARY_DARK_COLOR, brightness: Brightness.dark),
+);
